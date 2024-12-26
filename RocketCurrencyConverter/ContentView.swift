0bb3743af09a605a6eaf75daa425a68e2d40d012
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2024-12-26 20:56:50 -0300

    feat: add screen structure

--- RocketCurrencyConverter/ContentView.swift
+++ RocketCurrencyConverter/ContentView.swift
@@ -9,13 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            //Background image
+            
+            VStack {
+                //Logo image view
+                
+                //Currency exchange text
+                
+                //Conversion section
+                HStack {
+                    //Left conversion section
+                    VStack {
+                        //Currency
+                        HStack {
+                            //Currency image
+                            
+                            //Currency text
+                        }
+                        
+                        //Text field
+                    }
+                    
+                    //Equal sign
+                    
+                    //Right conversion section
+                    VStack {
+                        //Currency
+                        HStack {
+                            //Currency text
+                            
+                            //Currency image
+                        }
+                        
+                        //Text field
+                    }
+                }
+                
+                //Info button
+            }
         }
-        .padding()
     }
 }
 
