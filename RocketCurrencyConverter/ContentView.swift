ba95caf8a76f7e85a0dff5525d10f9a9b9b018c0
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2025-01-10 23:38:49 -0300

    feat: add coins

--- RocketCurrencyConverter/ContentView.swift
+++ RocketCurrencyConverter/ContentView.swift
@@ -11,11 +11,21 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             //Background image
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
             
             VStack {
                 //Logo image view
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
                 
                 //Currency exchange text
+                Text("Currency Exchange")
+                    .font(.title)
+                    .foregroundColor(Color.white)
                 
                 //Conversion section
                 HStack {
@@ -24,29 +34,54 @@ struct ContentView: View {
                         //Currency
                         HStack {
                             //Currency image
+                            Image(.silverpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 40)
                             
                             //Currency text
+                            Text("Silver piece")
+                                .font(.headline)
+                                .foregroundColor(Color.white)
                         }
                         
                         //Text field
+                        Text("Text field")
                     }
                     
                     //Equal sign
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                        .symbolEffect(.pulse)
                     
                     //Right conversion section
                     VStack {
                         //Currency
                         HStack {
                             //Currency text
+                            Text("Gold piece")
+                                .font(.headline)
+                                .foregroundColor(Color.white)
                             
                             //Currency image
+                            Image(.goldpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 40)
                         }
                         
                         //Text field
+                        Text("Text field")
                     }
                 }
                 
+                Spacer()
+                
                 //Info button
+                Image(systemName: "info.circle.fill")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
             }
         }
     }
